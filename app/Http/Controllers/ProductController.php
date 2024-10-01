e2f01aa264c907b95f1cdb313069d4d631a3160b
LeandroLDomingos <leandrolucasdomingos15@outlook.com>
2024-10-01 10:53:29 -0300

    Feat: 121

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -48,7 +48,9 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return inertia('Product/Show', [
+            'product' => ProductResource::make($product)
+        ]);
     }
 
     /**
